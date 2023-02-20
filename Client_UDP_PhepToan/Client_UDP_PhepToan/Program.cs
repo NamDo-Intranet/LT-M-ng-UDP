ba6ef2701d563586a2fc82bb23f747618fa044e1
nam <namdo4814@gmail.com>
2023-02-20 14:28:50 +0700

    Program UDP khong huong ket noi

---
+++ Client_UDP_PhepToan/Client_UDP_PhepToan/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Client_UDP_PhepToan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2014);
+            Socket skclient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            EndPoint enpt = (EndPoint)ipend;
+            string so1, so2, sum;
+            byte[] data;
+            int recv;
+            while (true)
+            {
+                Console.Write("Nhap so1: ");
+                so1 = Console.ReadLine();
+                if(so1.Length == 0) break;
+                data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(so1);
+                skclient.SendTo(data, ipend);
+
+                Console.Write("Nhap so2: ");
+                so2 = Console.ReadLine();
+                data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(so2);
+                skclient.SendTo(data, ipend);
+
+                data = new byte[1024];
+                recv = skclient.ReceiveFrom(data, ref enpt);
+                sum = Encoding.ASCII.GetString(data, 0, recv);
+                Console.Write("Tong cua (a={0} & b={1}) là: {2}\n", so1, so2, sum);
+            }
+            skclient.Close();
+        }
+    }
+}
---
+++ Server_UDP_PhepToan/Server_UDP_PhepToan/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_UDP_PhepToan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Any, 2014);
+            Socket skserver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            skserver.Bind(ipend);
+            EndPoint enpt = (EndPoint)ipend;
+
+            byte[] data;
+            string so1, so2, kq;
+            int recv;
+            while (true)
+            {
+                Console.Write("Waiting a: ");
+                data = new byte[1024];
+                recv = skserver.ReceiveFrom(data, ref enpt);
+
+                if (recv == 0) break;
+                so1 = Encoding.ASCII.GetString(data, 0, recv);
+
+                Console.Write("Waiting b: ");
+                data = new byte[1024];
+                recv = skserver.ReceiveFrom(data, ref enpt);
+
+                so2 = Encoding.ASCII.GetString(data, 0, recv);
+                Console.Write("Tong a={0}, b={1}", so1, so2);
+
+                kq = (Int32.Parse(so1)+Int32.Parse(so2)).ToString();
+
+                data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(kq);
+                skserver.SendTo(data, enpt);
+
+            }
+            skserver.Close();
+        }
+    }
+}
